4fba739898e9f97a05913e07738d92e3f2508c45
Abdullah Fareed <abdullah.fareed@paysyslabs.com>
2025-04-04 18:40:04 +0500

    db connected

--- api/index.js
+++ api/index.js
@@ -1,7 +1,11 @@
 import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+dotenv.config();
+const app = express();
 
-const app =express();
+connectDB();
 
-app.listen(3000, ()=>{
-    console.log(`server is running on port 3000`)
-})
+app.listen(3000, () => {
+  console.log(`server is running on port 3000`);
+});
