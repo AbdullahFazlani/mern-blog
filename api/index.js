773632211c14020d3285e2b5a135f21e5d41274d
Abdullah Fareed <abdullah.fareed@paysyslabs.com>
2025-04-06 01:23:28 +0500

    user route and controller created

--- api/index.js
+++ api/index.js
@@ -1,10 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
+import userRoutes from "./routes/user.route.js";
+
 dotenv.config();
 const app = express();
 
 connectDB();
+
+app.use("/api/user", userRoutes);
+
+app.get("/", (req, res) => {
+  res.json({ message: "api is running" });
+});
 
 app.listen(3000, () => {
   console.log(`server is running on port 3000`);
