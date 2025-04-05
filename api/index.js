ae225d56c2dd310f2e43736a75ebce2dd1639dc3
Abdullah Fareed <abdullah.fareed@paysyslabs.com>
2025-04-06 02:27:08 +0500

    signup backend done

--- api/index.js
+++ api/index.js
@@ -2,13 +2,16 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 const app = express();
+app.use(express.json());
 
 connectDB();
 
 app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
   res.json({ message: "api is running" });
